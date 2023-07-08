08e25c5310130fa4ea898dbe595536a075c42db2
Jacopo Siniscalco <jsiniscalco2000@gmail.com>
2023-07-08 14:19:13 +0100

    first commit

---
+++ src/include/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "options.h"
+
+static void init();
+static void finish(int);
+
+#endif // MAIN_H_
---
+++ src/include/options.h
@@ -0,0 +1,10 @@
+#ifndef OPTIONS_H_
+#define OPTIONS_H_
+
+#define DEBUG false
+#define STR_SIZE 128
+
+#define ENTER 13
+#define ESC 27
+
+#endif // OPTIONS_H_
